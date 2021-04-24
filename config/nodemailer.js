73fe3bf00ee778e22eeb1c3536295da9470e9757
samir sabiee <samirsabiee@gmail.com>
2021-04-24 10:35:41 +0430

    change email info

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -3,6 +3,7 @@ const nodeMailer = require('nodemailer')
 const transporter = nodeMailer.createTransport({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
+    secure: true, // true for 465, false for other ports
     auth: {
         user: process.env.MAIL_USERNAME, // generated ethereal user
         pass: process.env.MAIL_PASSWORD, // generated ethereal password
