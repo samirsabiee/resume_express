dbde9cc1a5b71cae4b17e38533144bc8cc91b731
samir sabiee <samirsabiee@gmail.com>
2021-04-25 20:40:57 +0430

    add related pages to right-sidebar

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,9 @@
 module.exports.dashboard = (req, res) => {
     res.render('admin/dashboard', {layout: 'index'})
 }
+module.exports.blog = (req, res) => {
+    res.render('admin/dashboard', {layout: 'blog'})
+}
+module.exports.comments = (req, res) => {
+    res.render('admin/dashboard', {layout: 'comments'})
+}
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,4 +2,6 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../controllers/admin')
 router.get('/dashboard', adminController.dashboard)
+router.get('/blog', adminController.blog)
+router.get('/comments', adminController.comments)
 module.exports = router
