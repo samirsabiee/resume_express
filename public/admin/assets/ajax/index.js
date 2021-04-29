33db1b5b589e3fcb371406ea9c0e88f98114b9f0
samir sabiee <samirsabiee@gmail.com>
2021-04-30 00:39:04 +0430

    add id attr to categoryName elem li, delete modal and ajax request to delete category

--- public/admin/assets/ajax/index.js
+++ public/admin/assets/ajax/index.js
@@ -70,6 +70,7 @@
                     let icon = $("#modalIcon")
                     icon.removeAttr('class')
                     icon.addClass('fas fa-2x fa-thumbs-down text-danger')
+                    console.log(jqXHR)
                     $("#messageModalContent")[0].innerText = jqXHR.responseJSON.message
                     $("#ajaxModal").modal("show")
                     $("#modalOkBtn").click(e => {
@@ -78,7 +79,54 @@
                 }
             })
         })
+        $(".deleteCategoryBtn").click(function (e) {
+            $("#deleteModal").modal('show')
+            let modalMessage = $("#deleteMessageModalContent")[0]
+            let categoryElement = getDeletingCategoryName(this)
+            let categoryName = categoryElement.textContent, categoryId = categoryElement.getAttribute('id')
+            modalMessage.innerHTML = `آیا از حذف دسته بندی <strong>${categoryName}</strong> مطمئن هستید؟`
+            $("#modalOkDeleteBtn").click(function (e) {
+                $('#deleteModal').modal('hide')
+                $.ajax({
+                    url: "http://localhost:3000/admin/category",
+                    data: {id: categoryId},
+                    method: "DELETE",
+                    contentType: "application/x-www-form-urlencoded", //and this
+                    success: function (response, textStatus, jqXHR) {
+                        let title = $("#ajaxModalLabel")
+                        title.addClass('text-success')
+                        title[0].textContent = 'موفق'
+                        let icon = $("#modalIcon")
+                        icon.removeAttr('class')
+                        icon.addClass('fas fa-2x fa-thumbs-up text-success')
+                        $("#messageModalContent")[0].innerText = response.message
+                        $("#ajaxModal").modal("show")
+                        $("#modalOkBtn").click(e => {
+                            $('#ajaxModal').modal('hide')
+                            window.location.reload();
+                        })
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        let title = $("#ajaxModalLabel")
+                        title.addClass('text-danger')
+                        title[0].textContent = 'ناموفق'
+                        let icon = $("#modalIcon")
+                        icon.removeAttr('class')
+                        icon.addClass('fas fa-2x fa-thumbs-down text-danger')
+                        $("#messageModalContent")[0].innerText = jqXHR.responseJSON.message
+                        $("#ajaxModal").modal("show")
+                        $("#modalOkBtn").click(e => {
+                            $('#ajaxModal').modal('hide')
+                        })
+                    }
+                })
+            })
+        })
     })
+
+    function getDeletingCategoryName(deleteIcon) {
+        return $(deleteIcon).parents()[1].firstElementChild.firstElementChild
+    }
 
     function createOptionElement(value, textContent) {
         return $(`<option value="${value}">${textContent}</option>`)
