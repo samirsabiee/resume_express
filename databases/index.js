cea43e6e61db0804167b6a5131cacdfae2106c8d
samir sabiee <samirsabiee@gmail.com>
2021-05-02 17:31:02 +0430

    change Database connection

--- databases/index.js
+++ databases/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
-module.exports = mongoose.connect(`${process.env.DB_URL}/${process.env.DB_NAME}`, {
+module.exports = mongoose.connect(`${process.env.DB_URL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
