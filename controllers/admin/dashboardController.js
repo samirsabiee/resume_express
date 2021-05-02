533c79851ff943d5a257add4232490ade03f9a45
samir sabiee <samirsabiee@gmail.com>
2021-05-02 16:47:59 +0430

    code optimization 9

---
+++ controllers/admin/dashboardController.js
@@ -0,0 +1,12 @@
+const articleModel = require('../../models/article')
+
+module.exports.dashboard = async (req, res) => {
+    try {
+        let data = {}
+        data.articleCounts = await articleModel.counts()
+        res.render('admin/dashboard', {layout: 'index', data})
+    } catch (e) {
+        res.status(404).send({message: e.message})
+    }
+}
+
