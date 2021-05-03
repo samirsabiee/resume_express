10409fe80c107566492f6b45db79ac4b9d5b8c4c
samir sabiee <samirsabiee@gmail.com>
2021-05-04 02:30:19 +0430

    add array of files validation using joi

---
+++ validation/files.js
@@ -0,0 +1,46 @@
+const Joi = require('joi')
+const {exception} = require('./errors')
+const media = Joi.array().items(Joi.object().keys({
+    fieldname: Joi
+        .string()
+        .required()
+        .error(errors => exception(errors, "fieldname"))
+        .label('FieldName'),
+    originalname: Joi
+        .string()
+        .required()
+        .error(errors => exception(errors, "originalname"))
+        .label('OriginalName'),
+    encoding: Joi
+        .string()
+        .required()
+        .error(errors => exception(errors, "encoding"))
+        .label('Encoding'),
+    mimetype: Joi
+        .string()
+        .required()
+        .error(errors => exception(errors, "mimetype"))
+        .label('MimeType'),
+    destination: Joi
+        .string()
+        .required()
+        .error(errors => exception(errors, "destination"))
+        .label('Destination'),
+    filename: Joi
+        .string()
+        .required()
+        .error(errors => exception(errors, "filename"))
+        .label('FileName'),
+    path: Joi
+        .string()
+        .required()
+        .error(errors => exception(errors, "path"))
+        .label('Path'),
+    size: Joi
+        .number()
+        .required()
+        .error(errors => exception(errors, "size"))
+        .label('Size')
+}).unknown(true))
+
+module.exports = media
