c909e6e7d99729c57d4a42437fc6b4f272078d90
samir sabiee <samirsabiee@gmail.com>
2021-05-04 03:24:23 +0430

    add success save sample message

--- services/messages.js
+++ services/messages.js
@@ -1,6 +1,7 @@
 module.exports = {
     successSendEmail: 'ایمیل با موفقیت ارسال شد',
     successSaveArticle: 'مقاله با موفقیت ذخیره شد',
+    successSaveSample: 'نمونه کار با موفقیت ذخیره شد',
     successEditArticle: 'مقاله با موفقیت ویرایش شد',
     unselectedImage: 'حداقل یک تصویر برای ذخیره مقاله انتخاب شود',
     routeNotFound: 'مسیر پیدا نشد',
